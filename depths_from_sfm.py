2c2c1ef102d76efecba2e26fd53e501bdc73e8b7
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2024-10-29 13:08:23 +0100

    tbs

--- depths_from_sfm.py
+++ depths_from_sfm.py
@@ -1,4 +1,5 @@
 import os.path
+import pathlib
 from pathlib import Path
 
 import numpy as np
@@ -8,15 +9,37 @@ import pycolmap
 from tqdm import tqdm
 
 
-# FIXME account for distortion
-# FIXME better split
-# FIXME depth in meters?
-# TODO calibration matrix
-def run(in_dir, out_dir, split, append, write, read_again=False):
+def get_data_file_key(image, conflict_names):
+    depth_data_file_key = Path(image.name).name[:-4]
+    if conflict_names.__contains__(depth_data_file_key):
+        for i in range(1000):
+            depth_data_file_key_new = f"{depth_data_file_key}_{i}"
+            if not conflict_names.__contains__(depth_data_file_key_new):
+                depth_data_file_key = depth_data_file_key_new
+                break
+        if conflict_names.__contains__(depth_data_file_key):
+            raise ValueError("cannot come up with a unique key")
+    conflict_names.add(depth_data_file_key)
+    return depth_data_file_key
+
+
+# TODO account for distortion
+# TODO better split
+# TODO depth in meters?
+# TODO check the track length etc...
+def run(in_dir,
+        out_dir_data,
+        out_dir_images,
+        append,
+        conflict_names,
+        rel_out_dir,
+        max_items=None,
+        read_again=False,
+        write=True):
     """
     How it works:
 
-    * provide int/out_dir or scene (in => megascenes.../scene/... out => Marigold/data/sfm/{scene})
+    * provide int/out_dir or scene (e.g. in => megascenes.../scene/... out => Marigold/data/sfm/{scene})
     * the .npz files contain keys 'uv' and 'camera_coords'
 
     * creates:
@@ -42,102 +65,147 @@ def run(in_dir, out_dir, split, append, write, read_again=False):
                     img3.npz
                eval.txt
 
-    :param in_dir:
-    :param out_dir:
+    :param in_dir: e.g., ../datasets/megascenes/{scene}/reconstruct/colmap/0
+    :param out_dir_data: e.g., ../Marigold/data/sfm/data/{scene}  or ./data/sfm/one_off
     :param split:
     :param append:
     :param write:
     :param read_again:
     :return:
     """
-    split_dir = os.path.join(out_dir, split)
-    split_data_dir = os.path.join(out_dir, split, "data")
-    Path(split_data_dir).mkdir(parents=True, exist_ok=True)
+    assert not read_again or write
+    np_out_dir = os.path.join(out_dir_data, "np")
+    Path(np_out_dir).mkdir(parents=True, exist_ok=True)
+    if rel_out_dir:
+        rel_out_dir = os.path.join(rel_out_dir, "np")
+    else:
+        rel_out_dir = os.path.join(out_dir_data, "np")
+    txt_file = os.path.join(out_dir_data, f"data.txt")
 
-    ds_txt_file = os.path.join(split_dir, f"{split}.txt")
-    print(f"Will write to {ds_txt_file}")
-    with open(ds_txt_file, "a" if append else "w") as ds_file:
+    print(f"{in_dir} => {np_out_dir}")
+    print(f"rel out dir (np): {rel_out_dir}")
+    print(f"out dir images: {out_dir_images}")
+    print(f"will write to: {txt_file}")
+    with open(txt_file, "a" if append else "w") as ds_file:
 
         reconstruction = pycolmap.Reconstruction(in_dir)
+        pycolmap.undistort_images()
         print("Reconstruction summary:")
         print(reconstruction.summary())
 
         valid_points = 0
         image_items = list(reconstruction.images.items())
+        if max_items:
+            image_items = image_items[:max_items]
         print(f"Number of images: {len(image_items)}")
-        for image_id, image in tqdm(image_items):
+        for _, image in tqdm(image_items):
+
+            camera = reconstruction.camera(image.camera_id)
+            cmodel = camera.model.name
+            assert cmodel == "SIMPLE_RADIAL"
+            # model camera_id width height f cx cy k
+            camera_params = f"{cmodel} {camera.camera_id} {camera.width} {camera.height} {' '.join([str(p) for p in reconstruction.camera(image.camera_id).params])}"
 
             uv_l = []
             camera_coords_l = []
+            # iterate through (valid 2D points)
             for p2d in image.points2D:
+
                 # NOTE: p2d.point3D_id = 18446744073709551615 (== -1 with the right arithmetic)
                 if p2d.point3D_id > 10 ** 11:
                     continue
+
+                # world coords => camera coords
                 xyz_w = reconstruction.points3D[p2d.point3D_id].xyz
-                uv_l.append(p2d.xy)
-                # TODO depth in meters?
                 camera_coords = image.cam_from_world * xyz_w
                 assert camera_coords[2] > 0
-                camera_coords_l.append(camera_coords)
-                camera = reconstruction.camera(image.camera_id)
-                cmodel = camera.model.name
-                assert cmodel == "SIMPLE_RADIAL"
-                # model camera_id width height f cx cy k
-                camera_params = f"{cmodel} {camera.camera_id} {camera.width} {camera.height} {' '.join([str(p) for p in reconstruction.camera(image.camera_id).params])}"
-                # camera_params_back = [float(i) for i in camera_params.split()[1:]]
 
-            depth_data_path = os.path.join(split_data_dir, f"{Path(image.name).name[:-4]}.npz")
+                uv_l.append(p2d.xy)
+                camera_coords_l.append(camera_coords)
+
+            depth_data_file_key = get_data_file_key(image, conflict_names)
+            depth_data_path = os.path.join(np_out_dir, f"{depth_data_file_key}.npz")
+            depth_data_rel_path = os.path.join(rel_out_dir, f"{depth_data_file_key}.npz")
             uv = np.array(uv_l)
             valid_points += len(uv_l)
             camera_coords = np.array(camera_coords_l)
             if write:
                 np.savez_compressed(depth_data_path, uv=uv, camera_coords=camera_coords)
-                jpg_data_path = os.path.join("..", image.name)
-                ds_file.write(f"{jpg_data_path} data/{Path(image.name).name[:-4]}.npz {camera_params}\n")
+                rgb_path = str(pathlib.Path(image.name).relative_to(pathlib.Path("commons")))
+                rgb_path = os.path.join(out_dir_images, rgb_path)
+                ds_file.write(f"{rgb_path} {depth_data_rel_path} {camera_params}\n")
+            # TEST
             if read_again:
                 read_back = np.load(depth_data_path)
                 uv_back = read_back["uv"]
                 assert np.all(uv == uv_back)
                 camera_coords_back = read_back["camera_coords"]
                 assert np.all(camera_coords == camera_coords_back)
-        print()
 
     print(f"Valid points: {valid_points}")
 
 
-if __name__ == '__main__':
-
-    # (input_dir, output_dir) => easy, for testing
-    # scenes -> list of scenes
-    #
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--scene", help="scene (e.g., prasna_brana)", required=False)
-    parser.add_argument("--input_dir", help="path to input model dir", required=False)
-    parser.add_argument("--output_dir", help="path to output dir", required=False)
-    args = parser.parse_args()
-
-    if args.scene is None and (args.input_dir is None or args.output_dir is None):
-        raise ValueError("Missing --scene and (--input_dir or --output_dir)")
-
-    if args.output_dir is None:
-        output_dir = f"../Marigold/data/sfm/{args.scene}"
-    else:
-        output_dir = args.output_dir
-    print(f"Output dir: {output_dir}")
+def run_for_scene(args):
+    """
+    ... or for one-off input dir
+    :param args:
+    :return:
+    """
 
     if args.input_dir is None:
-        rec_root = os.path.join(f"../datasets/megascenes", args.scene, "reconstruct/colmap/")
-        rec_dirs = sorted(list(Path(rec_root).glob("*")))
-        rec_dirs = [str(rec_dir) for rec_dir in rec_dirs if rec_dir.is_dir()]
-        dir_list = "\n".join([r for r in rec_dirs])
-        print(f"Found reconstruction directories for scene '{args.scene}': {dir_list}")
-    else:
-        rec_dirs = [args.input_dir]
-        print(f"Will use reconstruction directory: {rec_dirs}")
+        for scene in args.scenes:
+            print(f"Processing scene {scene} ...")
+            rec_root = os.path.join(f"../datasets/megascenes", scene, "reconstruct/colmap/")
+            reconstruction_dirs = sorted(list(Path(rec_root).glob("*")))
+            reconstruction_dirs = [str(rec_dir) for rec_dir in reconstruction_dirs if rec_dir.is_dir()]
+            dir_list = "\n".join([r for r in reconstruction_dirs])
+            print(f"Found reconstruction directories: {dir_list}\n")
 
-    # TODO find a better split strategy
-    val_split = int(len(rec_dirs) * 0.2)
-    for i, rec_dir in enumerate(rec_dirs):
-        split = "val" if i < val_split else "train"
-        run(rec_dir, output_dir, split, len(rec_dirs) != 1, write=False, read_again=True)
+            conflict_names = set()
+            output_dir_data = f"../Marigold/data/sfm/data/{scene}"
+            output_dir_imgs = f"images/{scene}"
+            rel_out_dir = str(pathlib.Path(output_dir_data).relative_to(pathlib.Path("../Marigold/data/sfm")))
+            for i, rec_dir in enumerate(reconstruction_dirs):
+                run(rec_dir,
+                    output_dir_data,
+                    output_dir_imgs,
+                    i != 0,
+                    conflict_names,
+                    rel_out_dir,
+                    args.max_items,
+                    write=True,
+                    read_again=True)
+
+        print("create the symlinks")
+        print("cd /datagrid/personal/vavravac/Marigold/data/sfm")
+        for scene in args.scenes:
+            print(f"ln -s ../../../../datasets/megascenes/{scene}/images/commons ./images/{scene}")
+
+    else:
+        run(args.input_dir,
+            args.output_dir,
+            args.output_dir,
+            False,
+            set(),
+            None,
+            args.max_items,
+            write=True,
+            read_again=True)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scenes", nargs="+", help="scene (e.g., prasna_brana)", required=False)
+    parser.add_argument("--input_dir", help="path to input model dir", required=False)
+    parser.add_argument("--output_dir", help="path to output dir", required=False)
+    parser.add_argument("--max_items", help="max images per reconstruction", type=int, required=False, default=None)
+    args = parser.parse_args()
+
+    if args.scenes is None and (args.input_dir is None or args.output_dir is None):
+        raise ValueError("Missing --scene and (--input_dir or --output_dir)")
+
+    if args.scenes is not None and (args.input_dir is not None or args.output_dir is not None):
+        raise ValueError("--scene and (--input_dir or --output_dir) both present")
+
+    run_for_scene(args)
---
+++ sfm_splits.py
@@ -0,0 +1,56 @@
+import glob
+import os.path
+import random
+
+import numpy as np
+import argparse
+
+from tqdm import tqdm
+
+
+def run_for_scene(scenes):
+    if scenes[0].lower() == "all":
+        paths = sorted(list(glob.glob("../Marigold/data/sfm/data/*/data.txt")))
+    else:
+        paths = [f"../Marigold/data/sfm/data/{scene}/data.txt" for scene in scenes]
+
+    print("Paths:\n", "\n".join(paths))
+
+    # TODO for now only random splits....
+    prefix = "all_"
+    split_map = {"train": 0.8, "eval": 0.1, "test": 0.1}
+    assert np.isclose(sum([split_map[k] for k in split_map.keys()]), 1.0)
+    print(f"input split map: {split_map}")
+
+    cum = 0.0
+    check_1 = False
+    for k in split_map.keys():
+        cum += split_map[k]
+        f = open(os.path.join("../Marigold/data/sfm", f"{prefix}{k}.txt"), "w")
+        split_map[k] = (cum, f)
+        assert cum < 1.000001
+        if np.isclose(cum, 1.0):
+            check_1 = True
+
+    assert check_1
+    print(f"cumulative map with file handlers: {split_map}")
+
+    for path in paths:
+        print(f"reading {path}")
+        with open(path, "r") as f:
+            for line in tqdm(f.readlines()):
+                r = random.random()
+                for split in split_map.keys():
+                    if r <= split_map[split][0]:
+                        split_map[split][1].write(line)
+                        break
+
+    for split in split_map.keys():
+        split_map[split][1].close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scenes", nargs="+", help="scene (e.g., 'prasna_brana karluv most')", required=True)
+    args = parser.parse_args()
+    run_for_scene(args.scenes)
