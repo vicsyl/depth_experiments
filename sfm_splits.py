6a69155d68301abe2fac9989c815b4f034e7f3ed
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2024-11-26 23:49:40 +0100

    sfm_splits configurable

--- sfm_splits.py
+++ sfm_splits.py
@@ -8,7 +8,7 @@ import argparse
 from tqdm import tqdm
 
 
-def run_for_scene(scenes):
+def run_for_scene(scenes, max_per_scene, prefix):
 
     # CMP
     # marigold_rel_path = "../Marigold"
@@ -23,7 +23,6 @@ def run_for_scene(scenes):
     print("Paths:\n", "\n".join(paths))
 
     # TODO for now only random splits....
-    prefix = "big_"
     split_map = {"train": 0.8, "eval": 0.1, "test": 0.1}
     assert np.isclose(sum([split_map[k] for k in split_map.keys()]), 1.0)
     print(f"input split map: {split_map}")
@@ -44,7 +43,10 @@ def run_for_scene(scenes):
     for path in paths:
         print(f"reading {path}")
         with open(path, "r") as f:
-            for line in tqdm(f.readlines()):
+            lines = f.readlines()
+            if max_per_scene is not None:
+                lines = lines[:max_per_scene]
+            for line in tqdm(lines):
                 r = random.random()
                 for split in split_map.keys():
                     if r <= split_map[split][0]:
@@ -58,5 +60,7 @@ def run_for_scene(scenes):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--scenes", nargs="+", help="scene (e.g., 'prasna_brana karluv most')", required=False, default=["all"])
+    parser.add_argument("--max_per_scene", type=int, help="max items per scene", required=False, default=None)
+    parser.add_argument("--prefix", type=str, help="splits prefix (i.e. all)", required=False, default="all")
     args = parser.parse_args()
-    run_for_scene(args.scenes)
+    run_for_scene(args.scenes, args.max_per_scene, args.prefix)
